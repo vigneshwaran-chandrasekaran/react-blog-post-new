e17f255972c9c343d90879c4046a6ea6641edca3
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2019-11-10 23:26:51 +0530

    Updates

---
+++ precache-manifest.1acb3613b1e288b1f149e480d3b86d7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5266640c6b04ee9bfaa0f0a7d4a65a",
+    "url": "/react-blog-post-new/index.html"
+  },
+  {
+    "revision": "79d96b920e89ae6521f2",
+    "url": "/react-blog-post-new/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "79d96b920e89ae6521f2",
+    "url": "/react-blog-post-new/static/js/2.19f4716f.chunk.js"
+  },
+  {
+    "revision": "387837c9b70f1279fd6b",
+    "url": "/react-blog-post-new/static/js/main.c3ba857d.chunk.js"
+  },
+  {
+    "revision": "548b6328ee76c25be2ef",
+    "url": "/react-blog-post-new/static/js/runtime-main.2ddcb18b.js"
+  }
+]);
